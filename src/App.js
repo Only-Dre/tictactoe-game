1c78dfe74d88273175ba84bfa433e32d90823f7b
Only-Dre <workofdre@proton.me>
2025-08-25 21:00:27 -0300

    NEW:  App.js com o primeiro Square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+// Criação do Elemento Square que irá compor o tabuleiro
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Square(){
+  // Componente possui function e return
+  return(
+    // Padrão square no botão
+    <button classname="square"></button>
+  )
 }
-
-export default App;
