3057706d2674a63a75cd21337f7d1fb1f41da8ad
Only-Dre <workofdre@proton.me>
2025-08-25 21:19:45 -0300

    NEW: Criação do tabuleiro 3x3 no App.js a partir das DIVs

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ export default function Square(){
   // Componente possui function e return
   return(// Padrão square no botão - Utiliza-se DIV pois "return" não pode ter mais de um item
     <div>
-    <button classname="square">1</button>
-    <button classname="square">2</button>
-    <button classname="square">3</button>
-    <button classname="square">4</button>
-    <button classname="square">5</button>
-    <button classname="square">6</button>
-    <button classname="square">7</button>
-    <button classname="square">8</button>
-    <button classname="square">9</button>
+      <div>
+        <button classname="square">1</button>
+        <button classname="square">2</button>
+        <button classname="square">3</button>
+      </div>
+      <div>
+        <button classname="square">4</button>
+        <button classname="square">5</button>
+        <button classname="square">6</button>
+      </div>
+      <div>
+        <button classname="square">7</button>
+        <button classname="square">8</button>
+        <button classname="square">9</button>
+      </div>
     </div>
   )
 }
