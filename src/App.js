2d23df04bc7012fbbcd0537a6f6843f1dd7af05b
Only-Dre <workofdre@proton.me>
2025-08-25 21:14:40 -0300

    NEW: Adição de nove botões no App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // Criação do Elemento Square que irá compor o tabuleiro
 import './App.css';
 
+// Criando botões
 export default function Square(){
   // Componente possui function e return
-  return(
-    // Padrão square no botão
-    <button classname="square"></button>
+  return(// Padrão square no botão - Utiliza-se DIV pois "return" não pode ter mais de um item
+    <div>
+    <button classname="square">1</button>
+    <button classname="square">2</button>
+    <button classname="square">3</button>
+    <button classname="square">4</button>
+    <button classname="square">5</button>
+    <button classname="square">6</button>
+    <button classname="square">7</button>
+    <button classname="square">8</button>
+    <button classname="square">9</button>
+    </div>
   )
 }
