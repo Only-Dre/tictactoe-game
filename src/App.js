a21b01a86bd97d0d902a274430d9511877d542ca
= <=>
2025-08-25 22:37:42 -0300

    NEW: Atualização de email e username

--- src/App.js
+++ src/App.js
@@ -16,23 +16,33 @@ function Square({valor, onSquareClick}){ // Square volta a ter propriedades, ago
 
 // Criando botões
 export default function Tabuleiro(){
+  // Criação de 9 espaços com Array
+  const [squares, setSquares] = useState(Array(9).fill(null)); // Preenchimento de Array
+  
+  function handleClick(i){ // i = índice
+    const nextSquares = squares.slice(); // Irá guardar em "nextSquares" uma cópia do Array
+    // Acessando índice
+    nextSquares[i] = "X"
+    setSquares(nextSquares); // Guarda a mudança
+  }
+
   // Componente possui function e return
   return(// Padronizando Square como um botão
     <div> {/* Criando GRIDS por Div para o jogo da velha */}
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div> {/* Números de Array em squares */}
+        <Square valor={squares[0] onSquareClick={handleClick(1)} />
+        <Square valor={squares[1] onSquareClick={handleClick(2)}} />
+        <Square valor={squares[2] onSquareClick={handleClick(3)}} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square valor={squares[3] onSquareClick={handleClick(3)}} />
+        <Square valor={squares[4] onSquareClick={handleClick(4)}} />
+        <Square valor={squares[5] onSquareClick={handleClick(5)}} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square valor={squares[6] onSquareClick={handleClick(6)}} />
+        <Square valor={squares[7] onSquareClick={handleClick(7)}} />
+        <Square valor={squares[8] onSquareClick={handleClick(8)}} />
       </div>
     </div>
   )
