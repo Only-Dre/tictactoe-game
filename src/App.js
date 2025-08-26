012cc7ad9bc352a7b004d1f1dc4de8f3b10a7300
= <=>
2025-08-25 22:47:09 -0300

    NEW: Adição de Array e Modificação de Squares nas DIVs com onSquareClick

--- src/App.js
+++ src/App.js
@@ -30,19 +30,19 @@ export default function Tabuleiro(){
   return(// Padronizando Square como um botão
     <div> {/* Criando GRIDS por Div para o jogo da velha */}
       <div> {/* Números de Array em squares */}
-        <Square valor={squares[0] onSquareClick={handleClick(1)} />
-        <Square valor={squares[1] onSquareClick={handleClick(2)}} />
-        <Square valor={squares[2] onSquareClick={handleClick(3)}} />
+        <Square valor={squares[0]} onSquareClick={() => {handleClick(0)}}/>
+        <Square valor={squares[1]} onSquareClick={() => {handleClick(1)}} />
+        <Square valor={squares[2]} onSquareClick={() => {handleClick(2)}} />
       </div>
       <div>
-        <Square valor={squares[3] onSquareClick={handleClick(3)}} />
-        <Square valor={squares[4] onSquareClick={handleClick(4)}} />
-        <Square valor={squares[5] onSquareClick={handleClick(5)}} />
+        <Square valor={squares[3]} onSquareClick={() => {handleClick(3)}} />
+        <Square valor={squares[4]} onSquareClick={() => {handleClick(4)}} />
+        <Square valor={squares[5]} onSquareClick={() => {handleClick(5)}} />
       </div>
       <div>
-        <Square valor={squares[6] onSquareClick={handleClick(6)}} />
-        <Square valor={squares[7] onSquareClick={handleClick(7)}} />
-        <Square valor={squares[8] onSquareClick={handleClick(8)}} />
+        <Square valor={squares[6]} onSquareClick={() => {handleClick(6)}} />
+        <Square valor={squares[7]} onSquareClick={() => {handleClick(7)}} />
+        <Square valor={squares[8]} onSquareClick={() => {handleClick(8)}} />
       </div>
     </div>
   )
